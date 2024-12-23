5ef3b3636d0a2633ab8aba14744216c9a3657f89
Nibazzador <88927707+Nibazzador@users.noreply.github.com>
2024-12-23 16:15:54 +0200

    New sites and comments

--- content.js
+++ content.js
@@ -1,9 +1,9 @@
 const blockedChoice = [
   "areena.yle.fi",
   "www.hltv.org",
-  "blast.tv",
   "www.twitch.tv",
   "www.youtube.com",
+  "yle.fi",
 ];
 
 const blockedAlways = [
@@ -17,19 +17,30 @@ const blockedAlways = [
   "www.facebook.com",
   "fi-fi.facebook.com",
   "twitter.com",
+  "x.com",
+  "blast.tv",
   "www.instagram.com",
   "www.reddit.com",
+  "slowroads.io",
+  "driftmas.slowroads.io",
+  "www.bubbleshooter.com",
+  "www.last.fm",
 ];
 
 if (blockedChoice.includes(window.location.hostname)) {
+  // If site url is in blockedChoise list:
   chrome.storage.local.get(["somesAllowed"]).then((somesAllowed) => {
+    // Gets info from chrome storage when user allowed optional sites last time
     if ((Date.now() - somesAllowed.somesAllowed) / 3600000 > 1) {
+      // blocks optional sites if more than an hour has elapsed
       chrome.storage.local.set({ block: true });
     }
     chrome.storage.local.get(["block"]).then((result) => {
+      // Gets info from chrome storage if optional sites are allowed
       resultBoolean = result.block;
 
       if (resultBoolean) {
+        // if true (sites blocked) -> redirect to break.html
         window.location.assign(chrome.runtime.getURL("break.html"));
       }
     });
@@ -37,5 +48,6 @@ if (blockedChoice.includes(window.location.hostname)) {
 }
 
 if (blockedAlways.includes(window.location.hostname)) {
-  window.location.assign(chrome.runtime.getURL("blockedAlways.html"));
+  // If site url is in blockedAlways list:
+  window.location.assign(chrome.runtime.getURL("blockedAlways.html")); // Redirect to blockedAlways site
 }
